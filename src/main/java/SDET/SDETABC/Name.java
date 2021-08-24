3618c1a32c81f7715ec2b0fa1be5619c2619275d
Akasha77777 <akash.as.shree@gmail.com>
2021-08-24 16:14:36 +0530

    created master

---
+++ src/main/java/SDET/SDETABC/Name.java
@@ -0,0 +1,9 @@
+package SDET.SDETABC;
+
+public class Name {
+	public void main(String args[])
+	{
+		System.out.println("ABC");
+		
+	}
+}
---
+++ src/main/java/SDET/SDETABC/Name1.java
@@ -0,0 +1,9 @@
+package SDET.SDETABC;
+
+public class Name1 {
+	public void main(String args[])
+	{
+		System.out.println("ABC");
+		
+	}
+}
